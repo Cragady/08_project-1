92474f6825f4f14156b07b16bc7dc0f885cfd172
Cragady <pinkdotdreamer@gmail.com>
2018-06-05 23:38:15 -0600

    get variables in firebase, and have asynchronous functions use them

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,3 +1,39 @@
-console.log("Hello, World!");
-console.log("this is a test for a pull request");
-console.log("secondary test");
+var config = {
+    apiKey: "AIzaSyDBvYQ7mCwkZnL6eMdy9kqQ-wSB9aySqMw",
+    authDomain: "chef-in-your-pantry.firebaseapp.com",
+    databaseURL: "https://chef-in-your-pantry.firebaseio.com",
+    projectId: "chef-in-your-pantry",
+    storageBucket: "chef-in-your-pantry.appspot.com",
+    messagingSenderId: "982672855907"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+var recipe;
+var bars;
+var pullSwitch = false;
+
+database.ref().once('value', function(snapshot){
+    recipe = snapshot.val().recipe;
+    bars = snapshot.val().bars;
+    pullSwitch = true;
+    console.log(pullSwitch);
+}).then(function(){ajaxCaller();});
+
+function ajaxCaller(){
+    $.ajax({
+        url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?diet=vegetarian&excludeIngredients=coconut&instructionsRequired=false&intolerances=egg%2C+gluten&limitLicense=false&number=10&offset=0&query=olives&type=main+course",
+        method: "GET",
+
+        headers:{
+            "X-Mashape-Key": recipe,
+            "accept": "application/json"
+    }
+    }).then(function(response){
+        console.log(response);
+    });
+}
+
+$("#click-me").click(function(){
+    ajaxCaller();
+});

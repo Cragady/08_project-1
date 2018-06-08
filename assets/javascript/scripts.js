796dc46ef34cd933b5e21a194987e10bd0a879f4
Cragady <pinkdotdreamer@gmail.com>
2018-06-08 00:32:04 -0600

    added a lot of functionality, ajax call is executed on load after firebase sets api's for testing reasons, now adding button's stylings

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -12,16 +12,21 @@ var config = {
 var recipe;
 var bars;
 var pullSwitch = false;
+var tempArray = [];
+var foodsArray = [];
+var pantsArray = [];
 
 database.ref().on('value', function(snapshot){
     recipe = snapshot.val().recipe;
     bars = snapshot.val().bars;
     pullSwitch = true;
-});//.then(function(){ajaxCallerRec();});
+    pageStarter();
+    $('[data-toggle="tooltip"]').tooltip();
+});
 
 function ajaxCallerRec(){
     $.ajax({
-        url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?diet=vegetarian&excludeIngredients=coconut&instructionsRequired=false&intolerances=egg%2C+gluten&limitLicense=false&number=10&offset=0&query=olives&type=main+course",
+        url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?diet=vegetarian&excludeIngredients=coconut&instructionsRequired=false&intolerances=egg%2C+gluten&limitLicense=false&number=10&offset=0&query=olives%20C&type=main+course",
         method: "GET",
 
         headers:{
@@ -37,16 +42,93 @@ function ajaxCallerBar(){
     $.ajax({
         url: "https://cors.io/?https://api.upcdatabase.org/product/072999493033/" + bars,
         method: "GET",
+
     }).then(function(response){
-        console.log(response);
-    });
-    
+        var obj = JSON.parse(response);
+        var barcodeData = $("<button>")
+
+        tempArray = string_to_array(obj.title);
+        buttonSetterFunk();
+        });   
 }
 
-$("#click-me").click(function(){
-    ajaxCallerRec();
-});
+string_to_array = function (str) {
+    return str.trim().split(" ");
+};
 
-$("#click-me2").click(function(){
+buttonSetterFunk = function(){
+    
+    for(i = 0; i < tempArray.length; i++){
+        newBtn = $("<button>");
+        btnInfo = tempArray[i];
+        newBtn.text(btnInfo);
+        newBtn.attr({
+            "data-word": tempArray[i],
+            "class": "cray-selector"
+        });
+        $("#pantry-items-show").append(newBtn);
+
+    }
+};
+
+btnGrabber = function(){
+
+    $("body").on("click", "button.cray-selector", function(){
+        titleGrabber = $(this).attr("data-word");
+        statusGrabber = $(this).attr("data-selected");
+        if(titleGrabber !== undefined){
+            if(!foodsArray.includes(titleGrabber)){
+                foodsArray.push(titleGrabber);
+            }
+        };
+        if(statusGrabber !== undefined){
+            if(statusGrabber === "no"){
+                $(this).attr("data-selected", "yes");
+                
+            }
+        };
+    });
+}
+
+pantsSet = function(){
+    $("#pants-array-btn").click(function(){
+        arrItem = "";
+        arrItemShow = "";
+        if(foodsArray.length > 1){
+            for(i = 0; i < foodsArray.length; i++){
+                arrItem += foodsArray[i] + "%20C";
+                arrItemShow += foodsArray[i] + " ";
+            }
+        } else if (foodsArray.length === 1){
+            arrItem = foodsArray[0];
+            arrItemShow = foodsArray[0];
+        } else if(foodsArray.length === 0){
+            return;
+        };
+        console.log(arrItem);
+        newPantsItem = $("<button>");
+        newPantsItem.text(arrItemShow);
+        newPantsItem.attr({
+            "data-pantry": arrItem,
+            "class": "card p-1 cray-selector",
+            "data-selected": "no"
+        });
+        newPantsItem.css("background", "#c7cfdb");
+        $("#pantry-items").append(newPantsItem);
+        arrItem = "";
+        arrItemShow = "";
+    });
+};
+
+pageStarter = function(){
+    btnGrabber();
     ajaxCallerBar();
-})
+    pantsSet();
+}
+
+
+
+
+
+//%20C for spaces 
+//do concatenation for foodsArray from pantsArray

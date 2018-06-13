f1661f9a471dbcd7616ad756ba2e6c2f8e970bde
Brannon Laveder <blaveder1@gmail.com>
2018-06-12 18:44:54 -0600

    jquery button

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -5,9 +5,9 @@ var config = {
     projectId: "chef-in-your-pantry",
     storageBucket: "chef-in-your-pantry.appspot.com",
     messagingSenderId: "982672855907"
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 
 var recipe;
 var bars;
@@ -25,36 +25,62 @@ var manuScanu;
 
 /*sets the values required for the ajax calls
 then calls pageStarter() to activate button functionality*/
-database.ref().on('value', function(snapshot){
+database.ref().on('value', function (snapshot) {
     recipe = snapshot.val().recipe;
     bars = snapshot.val().bars;
     bars2 = snapshot.val().bars2;
     pageStarter();
 });
 
-function ajaxCallerRec(){
+function ajaxCallerRec() {
     $.ajax({
-        url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?diet=vegetarian&excludeIngredients=coconut&instructionsRequired=false&intolerances=egg%2C+gluten&limitLicense=false&number=10&offset=0&query=olives%20C&type=main+course",
+        url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=1&tags=noodles%2Ctomatoes",
+        // url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=1&tags=noodles%2Ctomatoes",
         method: "GET",
 
-        headers:{
+        headers: {
             "X-Mashape-Key": recipe,
             "accept": "application/json"
-    }
-    }).then(function(response){
+        }
+    }).then(function (response) {
+        console.log(response);
+        var recipeCard = $("#recipe-card");
+        var recipeText = $("<div>");
+        recipeText.attr("id", "recipe-text");
+        var recipeAddress = $("<div>");
+        recipeAddress.attr("id", "#recipe-address");
+        // recipeCard.text(response.recipes[0].title);
+        var title = response.recipes[0].title;
+        var recipeImg = $("<img>");
+        recipeImg.attr("src", response.recipes[0].image);
+        var time = response.recipes[0].readyInMinutes;
+        var address = response.recipes[0].spoonacularSourceUrl;
+
+        recipeCard.append(recipeImg);
+        recipeCard.append(recipeText);
+        recipeText.append(title);
+        recipeText.append(time);
+        recipeText.append(address);
+
+
     });
 }
+function showRecipe() {
+    $("#pants-array-set").click(function () {
+        ajaxCallerRec();
+    });
+};
 
-function ajaxCallerBar(newItemScan){
+function ajaxCallerBar(newItemScan) {
     $.ajax({
         url: "https://cors.io/?https://api.upcdatabase.org/product/" + newItemScan + "/" + bars,
         method: "GET",
 
-    }).then(function(response){
+    }).then(function (response) {
         var obj = JSON.parse(response);
         tempArray = string_to_array(obj.title);
         buttonSetterFunk();
-        });   
+    });
 }
 
 /*takes response from ajaxCallerBar() and makes it 
@@ -63,9 +89,9 @@ string_to_array = function (str) {
     return str.trim().split(" ");
 };
 
-buttonSetterFunk = function(){
-    
-    for(i = 0; i < tempArray.length; i++){
+buttonSetterFunk = function () {
+
+    for (i = 0; i < tempArray.length; i++) {
         newBtn = $("<button>");
         btnInfo = tempArray[i];
         newBtn.text(btnInfo);
@@ -80,68 +106,69 @@ buttonSetterFunk = function(){
     }
 };
 
-btnGrabber = function(){
+btnGrabber = function () {
 
     $("body").on({
-        mouseenter: function(){
+        mouseenter: function () {
             $(this).css("opacity", "0.75");
         },
-        mouseleave: function(){
+        mouseleave: function () {
             $(this).css("opacity", "1");
         },
-        click: function(){
-        titleGrabber = $(this).attr("data-word");
-        statusGrabber = $(this).attr("data-selected");
-        pantryGrabber = $(this).attr("data-pantry")
-        if((statusGrabber !== undefined) && (ingredientResetter === false) && (ingredientDeleter === false)){
-            if(statusGrabber === "no"){
-                $(this).attr("data-selected", "yes");
-                $(this).css("background", "#86d6d6");
-                if(pantryGrabber !== undefined){
-                    searchParamArray.push(pantryGrabber);
-                    console.log(searchParamArray);
+        click: function () {
+            titleGrabber = $(this).attr("data-word");
+            statusGrabber = $(this).attr("data-selected");
+            pantryGrabber = $(this).attr("data-pantry")
+            if ((statusGrabber !== undefined) && (ingredientResetter === false) && (ingredientDeleter === false)) {
+                if (statusGrabber === "no") {
+                    $(this).attr("data-selected", "yes");
+                    $(this).css("background", "#86d6d6");
+                    if (pantryGrabber !== undefined) {
+                        searchParamArray.push(pantryGrabber);
+                        console.log(searchParamArray);
+                    }
+                    if (titleGrabber !== undefined) {
+                        if (!foodsArray.includes(titleGrabber)) {
+                            foodsArray.push(titleGrabber);
+                            console.log(foodsArray);
+                        }
+                    }
+
                 }
-                if(titleGrabber !== undefined){
-                    if(!foodsArray.includes(titleGrabber)){
-                        foodsArray.push(titleGrabber);
+                if (statusGrabber === "yes") {
+                    $(this).attr("data-selected", "no");
+                    $(this).css("background", "#c7cfdb")
+                    if (pantryGrabber !== undefined) {
+                        searchParamArray.splice($.inArray(pantryGrabber, searchParamArray), 1);
+                        console.log("this this" + pantryGrabber);
+                        console.log(searchParamArray);
+                    }
+                    if (titleGrabber !== undefined) {
+                        foodsArray.splice($.inArray(titleGrabber, foodsArray), 1);
                         console.log(foodsArray);
                     }
                 }
-                
-            }
-            if(statusGrabber === "yes"){
-                $(this).attr("data-selected", "no");
-                $(this).css("background", "#c7cfdb")
-                if(pantryGrabber !== undefined){
-                    searchParamArray.splice($.inArray(pantryGrabber, searchParamArray),1);
-                    console.log("this this" +pantryGrabber);
-                    console.log(searchParamArray);
-                }
-                if(titleGrabber !== undefined){
-                    foodsArray.splice($.inArray(titleGrabber, foodsArray),1);
-                    console.log(foodsArray);
-                }
-            }
-        };
-        if((ingredientResetter === true) || (ingredientDeleter === true)){
-            modalChecker = $(this).attr("data-modal");
-            console.log(modalChecker);
-            if(modalChecker === "no"){
-                oldDataGrabber = $(this).text();
-
-                oldDataLayer(oldDataGrabber, splicedDiced);
             };
-        };
-    }}, "button.cray-selector");
+            if ((ingredientResetter === true) || (ingredientDeleter === true)) {
+                modalChecker = $(this).attr("data-modal");
+                console.log(modalChecker);
+                if (modalChecker === "no") {
+                    oldDataGrabber = $(this).text();
+
+                    oldDataLayer(oldDataGrabber, splicedDiced);
+                };
+            };
+        }
+    }, "button.cray-selector");
 }
 
-function hasValue(arrPusher){
-    for(var i = 0; i < pantsArray.length; i++){
+function hasValue(arrPusher) {
+    for (var i = 0; i < pantsArray.length; i++) {
         for (let value of Object.values(pantsArray[i])) {
-            if((value === arrPusher) && (ingredientResetter === false) && (ingredientDeleter === false)){
+            if ((value === arrPusher) && (ingredientResetter === false) && (ingredientDeleter === false)) {
                 sameValCheck = false;
-            } else if ((ingredientResetter === true) || (ingredientDeleter === true)){
-                if(value === arrPusher){
+            } else if ((ingredientResetter === true) || (ingredientDeleter === true)) {
+                if (value === arrPusher) {
                     tempArray = pantsArray[i].oldData.tempArray;
                     ingredientResetter = false;
                     ingredientDeleter = false;
@@ -153,8 +180,8 @@ function hasValue(arrPusher){
     }
 }
 
-pantsSet = function(){
-    $("#pants-array-btn").click(function(){
+pantsSet = function () {
+    $("#pants-array-btn").click(function () {
         pushToPantry();
         $("#my-modal").modal("toggle");
     });
@@ -164,52 +191,52 @@ pantsSet = function(){
 
 // });
 
-pushToPantry = function(){
+pushToPantry = function () {
     arrItem = "";
-        arrItemShow = "";
-        sameValCheck = true;
-        if(foodsArray.length > 0){
-            for(i = 0; i < foodsArray.length; i++){
-                arrItem += foodsArray[i] + "%20C";
-                arrItemShow += foodsArray[i] + " ";
-                orderPants = {arrItemShow: arrItemShow, arrItem, oldData:{tempArray}};
-            }
-            console.log(orderPants);
-        } else if (foodsArray.length === 0){
-            return;
-        };
-        hasValue(arrItemShow);
-        if(sameValCheck === true){
-            pantsArray.push(orderPants);
-            newPantsItem = $("<button>");
-            newPantsItem.text(arrItemShow);
-            newPantsItem.attr({
-                "data-pantry": arrItem,
-                "data-modal": "no",
-                "data-resetting": "no",
-                "data-deleting": "no",
-                "class": "card p-1 m-1 cray-selector second-cray",
-                "data-selected": "no"
-            });
-            newPantsItem.css("background", "#c7cfdb");
-            $("#pantry-items").append(newPantsItem);
-            itemResetArray = foodsArray;
-            arrItem = "";
-            arrItemShow = "";
-            $("div#pantry-items-show").empty();
-            foodsArray = [];
-            tempArray = [];
+    arrItemShow = "";
+    sameValCheck = true;
+    if (foodsArray.length > 0) {
+        for (i = 0; i < foodsArray.length; i++) {
+            arrItem += foodsArray[i] + "%20C";
+            arrItemShow += foodsArray[i] + " ";
+            orderPants = { arrItemShow: arrItemShow, arrItem, oldData: { tempArray } };
         }
+        console.log(orderPants);
+    } else if (foodsArray.length === 0) {
+        return;
+    };
+    hasValue(arrItemShow);
+    if (sameValCheck === true) {
+        pantsArray.push(orderPants);
+        newPantsItem = $("<button>");
+        newPantsItem.text(arrItemShow);
+        newPantsItem.attr({
+            "data-pantry": arrItem,
+            "data-modal": "no",
+            "data-resetting": "no",
+            "data-deleting": "no",
+            "class": "card p-1 m-1 cray-selector second-cray",
+            "data-selected": "no"
+        });
+        newPantsItem.css("background", "#c7cfdb");
+        $("#pantry-items").append(newPantsItem);
+        itemResetArray = foodsArray;
+        arrItem = "";
+        arrItemShow = "";
+        $("div#pantry-items-show").empty();
+        foodsArray = [];
+        tempArray = [];
+    }
 };
 
-resetPantryItems = function(){
-    $("button#reset-pants-item").click(function(){
+resetPantryItems = function () {
+    $("button#reset-pants-item").click(function () {
         thisButtonGrab = $("button#reset-pants-item");
         resetSelector = $("button.cray-selector.second-cray");
         resetStatus = thisButtonGrab.attr("data-resetting");
         deleteCheck = resetSelector.attr("data-deleting");
-        if(deleteCheck === "no"){
-            if(resetStatus === "no"){
+        if (deleteCheck === "no") {
+            if (resetStatus === "no") {
                 thisButtonGrab.attr("data-resetting", "yes");
                 thisButtonGrab.attr("data-deleting", "no");
                 resetSelector.css("background", "#f28aca");
@@ -219,8 +246,8 @@ resetPantryItems = function(){
                 ingredientDeleter = false;
             };
         };
-            
-        if(resetStatus === "yes"){
+
+        if (resetStatus === "yes") {
             thisButtonGrab.attr("data-resetting", "no");
             thisButtonGrab.attr("data-deleting", "no");
             resetSelector.css("background", "#c7cfdb");
@@ -228,18 +255,18 @@ resetPantryItems = function(){
             ingredientResetter = false;
             ingredientDeleter = false;
         };
-        
+
     });
 };
 
-deletePantryItems = function(){
-    $("button#delete-pants-item").click(function(){
+deletePantryItems = function () {
+    $("button#delete-pants-item").click(function () {
         thisButtonGrab = $("button#delete-pants-item");
         resetSelector = $("button.cray-selector.second-cray");
         resetStatus = thisButtonGrab.attr("data-deleting");
         resetCheck = resetSelector.attr("data-resetting");
-        if(resetCheck === "no"){
-            if(resetStatus === "no"){
+        if (resetCheck === "no") {
+            if (resetStatus === "no") {
                 thisButtonGrab.attr("data-deleting", "yes");
                 thisButtonGrab.attr("data-resetting", "no");
                 resetSelector.css("background", "#dd0837");
@@ -249,8 +276,8 @@ deletePantryItems = function(){
                 ingredientResetter = false;
             };
         };
-            
-        if(resetStatus === "yes"){
+
+        if (resetStatus === "yes") {
             thisButtonGrab.attr("data-deleting", "no");
             thisButtonGrab.attr("data-resetting", "no");
             resetSelector.css("background", "#c7cfdb");
@@ -258,17 +285,17 @@ deletePantryItems = function(){
             ingredientDeleter = false;
             ingredientResetter = false;
         };
-        
+
     });
 };
 
-typedItemEntry = function(){
-    $("button#quick-item-entry").click(function(event){
+typedItemEntry = function () {
+    $("button#quick-item-entry").click(function (event) {
         event.preventDefault();
         typedPantryItem = $("#input-password-2").val().trim();
         typedPantryItem = string_to_array(typedPantryItem);
         tempArray = typedPantryItem;
-        for(i = 0; i < typedPantryItem.length; i++){
+        for (i = 0; i < typedPantryItem.length; i++) {
             foodsArray.push(typedPantryItem[i]);
         }
         $("#input-password-2").val("");
@@ -276,26 +303,26 @@ typedItemEntry = function(){
     });
 };
 
-oldDataLayer = function(oldSelector, arrObjSplicer){
+oldDataLayer = function (oldSelector, arrObjSplicer) {
     oldBtnSelector2 = $("button:contains(" + oldSelector + ")");
     oldBtnSelector2.remove();
-    if(ingredientResetter === true){
+    if (ingredientResetter === true) {
         hasValue(oldSelector);
         buttonSetterFunk();
         $("#my-modal").modal("toggle");
     } else {
         hasValue(oldSelector);
     };
-    if(splicedDiced !== -1){
+    if (splicedDiced !== -1) {
         pantsArray.splice(splicedDiced, 1);
     }
     $("button.cray-selector").css("background", "#c7cfdb");
-    
+
 };
 
-previousIngredientsLister = function(){
-    if(pantsArray !== undefined){
-        for(i = 0; i < pantsArray.length; i++){
+previousIngredientsLister = function () {
+    if (pantsArray !== undefined) {
+        for (i = 0; i < pantsArray.length; i++) {
             oldPantsItem = $("<button>");
             oldPantsItem.text(pantsArray[i].arrItemShow);
             oldPantsItem.attr({
@@ -305,31 +332,31 @@ previousIngredientsLister = function(){
                 "data-old": pantsArray[i].oldData
             });
             oldPantsItem.css("background", "#c7cfdb");
-            $("#pantry-items").append(oldPantsItem);    
+            $("#pantry-items").append(oldPantsItem);
         }
     } else {
-    pantsArray = [];
+        pantsArray = [];
     };
 };
 
-scanButtonInput = function(){
-    $("#pills-home-tab-btn").click(function(){
+scanButtonInput = function () {
+    $("#pills-home-tab-btn").click(function () {
         $("#my-modal").modal("toggle");
     });
 };
 
-scannerAutoInput = function(){
-    $("#auto-scanner-input").on("keydown", function(e){
-        if (e.keyCode === 13){
+scannerAutoInput = function () {
+    $("#auto-scanner-input").on("keydown", function (e) {
+        if (e.keyCode === 13) {
             manuScanu = $("#auto-scanner-input").val();
             $("#auto-scanner-input").val("");
             $("#my-modal").modal("toggle");
             ajaxCallerBar(manuScanu);
-        };      
+        };
     });
 };
 
-pageStarter = function(){
+pageStarter = function () {
     previousIngredientsLister();
     resetPantryItems();
     deletePantryItems();
@@ -340,6 +367,8 @@ pageStarter = function(){
     pantsSet();
     scanButtonInput();
     scannerAutoInput();
+    showRecipe();
+    // ajaxCallerRec();
 }
 
 
